f85222916c751eb31034e4670c6afc04d1ba931c
git-aditi <aditithakkar1998@gmail.com>
2023-09-03 14:11:57 -0400

    First Commit

---
+++ StudentManagementSystem/src/main/java/com/company/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,30 @@
+package com.company.studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+	
+	public static void main(String[] args) {
+
+		
+		// Ask how many new users we want to add
+		System.out.println("Enter the number of students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
+		// Create n number of new students
+		for(int n=0; n<numOfStudents; n++) {
+			students[n]= new Student();
+			students[n].enroll();
+			students[n].payTuition();
+			
+		}
+		
+		for(int n = 0; n < numOfStudents; n++) {
+			System.out.println(students[n].toString());
+		}
+		
+	}
+
+}
